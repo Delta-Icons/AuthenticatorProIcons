19bff1613062ae73d7fe8b7d648bce278351db93
GitGitro <108683123+GitGitro@users.noreply.github.com>
2023-07-20 23:24:59 +0200

    Delta everywhere

--- build_aegis_icons.py
+++ build_aegis_icons.py
@@ -7,9 +7,9 @@ def main():
     settings = BasicBuildSettings()
     settings.repo_url = "https://github.com/Delta-Icons/aegis-icons.git"
     settings.svg_glob = ["icons", "**", "*.svg"]
-    settings.output_name = "aegis-icons"
-    settings.pack_name = "Aegis Icons"
-    settings.pack_description = "delta version of the unofficial monochrome-styled 2FA icons for open source Android authenticator Aegis"
+    settings.output_name = "delta-aegis-icons"
+    settings.pack_name = "Delta Aegis Icons"
+    settings.pack_description = "Delta version of the unofficial monochrome-styled 2FA icons for open source Android authenticator Aegis"
     settings.pack_url = "https://github.com/Delta-Icons/aegis-icons"
 
     build_basic_pack(settings)
