3a7dedbd4d01c90924c69c717d5125d7913d8ed9
GitGitro <108683123+GitGitro@users.noreply.github.com>
2023-07-20 22:56:10 +0200

    Update build_aegis_icons.py

--- build_aegis_icons.py
+++ build_aegis_icons.py
@@ -5,12 +5,12 @@ from util.basic_builder import BasicBuildSettings, build_basic_pack
 
 def main():
     settings = BasicBuildSettings()
-    settings.repo_url = "https://github.com/GitGitro/aegis-icons.git"
+    settings.repo_url = "https://github.com/Delta-Icons/aegis-icons.git"
     settings.svg_glob = ["icons", "**", "*.svg"]
     settings.output_name = "aegis-icons"
     settings.pack_name = "Aegis Icons"
-    settings.pack_description = "Unofficial monochrome-styled 2FA icons for open source Android authenticator Aegis"
-    settings.pack_url = "https://github.com/aegis-icons/aegis-icons"
+    settings.pack_description = "delta version of the unofficial monochrome-styled 2FA icons for open source Android authenticator Aegis"
+    settings.pack_url = "https://github.com/Delta-Icons/aegis-icons"
 
     build_basic_pack(settings)
 
